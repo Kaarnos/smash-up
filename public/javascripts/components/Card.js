bd74cedf24497ebdad3d180766a0cfa586deb1c8
Romain Aranda <romain.aranda@gmail.com>
2022-05-18 23:39:46 +0200

    js module structure

---
+++ public/javascripts/components/Card.js
@@ -0,0 +1,9 @@
+export default class Card {
+    constructor(id) {
+        this.id = id;
+    }
+
+    getId() {
+        return this.id;
+    }
+}
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,9 @@
 console.log("Hello world!");
+
+import Card from "./components/Card.js";
+
+const newCard = new Card(1);
+
+const cardId = newCard.getId();
+
+console.log("new card id is: ", cardId);
