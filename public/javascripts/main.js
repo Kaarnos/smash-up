477a0d2548d387aa0195b745e3a3ace7930aee57
Romain Aranda <romain.aranda@gmail.com>
2022-05-21 17:57:25 +0200

    organise css files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,23 @@
-// import Card from "./components/Card.js";
+import Card from "./components/Card.js";
 // import Button from "./components/Button.js";
 
-// const newCard = new Card(1);
+const newCard = new Card(1);
 
-// const cardId = newCard.getId();
-// console.log("new card id is: ", cardId);
+const cardId = newCard.getId();
+console.log("new card id is: ", cardId);
 
 
 // const newButton = new Button();
 
 // const handEl = document.getElementById('hand');
 // handEl.appendChild(newButton);
+
+// const playerDiscardIcons = document.getElementsByClassName('info-discard');
+
+$( ".info-discard" ).click(() => {
+    console.log('click on discard');
+})
+
+// playerDiscardIcon.addEventListener('click', () => {
+//     console.log('click on discard');
+// });
